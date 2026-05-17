a480f715b8b627b23e7ff646eb1c6eadc84edb3e
Krish Shah <114970376+kri-shah@users.noreply.github.com>
2026-05-17 16:08:54 -0400

    Add: design-word-search-data-structure - submission-1

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-1.py
@@ -0,0 +1,38 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.finished = False
+class WordDictionary:
+    def __init__(self):
+        self.trie = Trie()
+
+    def addWord(self, word: str) -> None:
+        curr = self.trie
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = Trie()
+            curr = curr.children[c]
+        curr.finished = True
+
+    def _search(self, curr, word, i):
+        print(i)
+        if i == len(word):
+            return curr.finished
+        if word[i] in curr.children:
+            if self._search(curr.children[word[i]], word, i + 1):
+                return True
+        elif word[i] == '.':
+            for child in curr.children.values():
+                if self._search(child, word, i + 1):
+                    return True
+        
+        return False
+
+    def search(self, word: str) -> bool:
+        return self._search(self.trie, word, 0)
+    
+       
+            
+
+
+
